4542dbb1126504835aad500bc7c3589f958a1b25
SaifGharbi <saif.gharbi@esprit.tn>
2025-02-23 13:16:10 +0100

    simple integration back

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+use App\Repository\ParcelleRepository;
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'admin_dashboard', methods: ['GET'])]
+    public function dashboard(): Response
+    {
+        return $this->render('Admin/dashboard.html.twig');
+    }
+
+    #[Route('/users', name: 'admin_users', methods: ['GET'])]
+    public function users(UserRepository $userRepository): Response
+    {
+        return $this->render('Admin/users.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/parcelles', name: 'admin_parcelles', methods: ['GET'])]
+    public function parcelles(ParcelleRepository $parcelleRepository): Response
+    {
+        return $this->render('Admin/parcelles.html.twig', [
+            'parcelles' => $parcelleRepository->findAll(),
+        ]);
+    }
+}
